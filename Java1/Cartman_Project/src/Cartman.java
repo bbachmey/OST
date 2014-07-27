53ef74ae8fa8c4a4e111af625d201f8ca29aec12
bachmeb <bachmeb@mail.uc.edu>
2014-07-26 22:34:30 -0400

    refactored Cartman

--- Java1/Cartman_Project/src/Cartman.java
+++ Java1/Cartman_Project/src/Cartman.java
@@ -13,13 +13,13 @@ public class Cartman {
 	/**
 	 * @param graph
 	 * This is the Cartman constructor
-	 * This constructor method requires a Graphics object and return a Cartman
+	 * This constructor method requires a Graphics object and returns a Cartman
 	 */
 	public Cartman(Graphics graph){
 		// put the Graphics object into an instance variable
 		this.g = graph;
 		// call the private method that creates each Cartman part
-		this.makeMe();
+		this.buildMe();
 	}
 	
 	
@@ -27,41 +27,19 @@ public class Cartman {
 	 * This method is called by the constructor
 	 * The order that Parts are added to the ArrayList is the order in which they are drawn
 	 */
-	private void makeMe(){	
-		//head
-		Part head = new Head(
-				Color.PINK, 
-				10, 	//x
-				30, 	//y
-				180, 	//w
-				150);	//h
-		parts.add(head);
+	private void buildMe(){	
 		
-		//chin
-		Part chin = new Chin(
-				Color.BLACK, 
-				((Head)head).getxPos()+25, 	//x
-				((Head)head).getyPos()+100, 		//y
-				((Head)head).getWidth()-(((Head)head).getxPos()*5),  	//w
-				((Head)head).getHeight() / 4, 	//h
-				-0, 	//start angle
-				-180);	//arc angle
-		parts.add(chin);
+		parts.add(makeHead());
+		//
+		parts.add(makeChin());
+		//
+		parts.add(makeTooth());
 		
 		//Part leftEye = new Eye();
 		//Part rightEye = new Eye();
 		//Part leftEyeball = new EyeBall();
 		//Part rightEyeball = new EyeBall();
 		//Part mouth = new Mouth();
-		
-		Part tooth = new Tooth(
-				Color.WHITE,
-				((Head)head).getxPos()+75,		//x position
-				((Head)head).getyPos()+75		//y position
-				);		
-		//add the tooth to the parts ArrayList
-		parts.add(tooth);
-		
 		//Part body = new Body();
 		//Part leftFoot = new Foot();
 		//Part rightFoot = new Foot();
@@ -75,6 +53,22 @@ public class Cartman {
 
 	}
 
+
+	public void drawAllOfMe(){
+		//Polymorphism in action!
+		//	The draw() method of each object in the ArrayList<Part> parts is called
+		//	and Java knows to execute the @Override draw() method of the subclass object 
+		// 	in the ArrayList that extends the Part class
+		for(Part p : parts){
+			//get the color from the Part object and set the color of the Graphics object
+			g.setColor(p.getColor());
+			//call the draw() method of the Part
+			p.draw(g);
+		}
+		
+	}
+
+
 	/**
 	 * @param partNum
 	 * This method takes an integer and and calls the draw() method of an item in the ArrayList<Part> parts
@@ -85,18 +79,48 @@ public class Cartman {
 		
 		}
 
-	public void drawAllOfMe(){
-		//Polymorphism in action!
-		//	The draw() method of each object in the ArrayList<Part> parts is called
-		//	and Java knows to execute the @Override draw() method of object in the ArrayList
-		//	that extends the Part class
-		for(Part p : parts){
-			//get the color from the Part and set the color of the Graphics object
-			g.setColor(p.getColor());
-			//call the draw() method of the Part
-			p.draw(g);
-		}
-		
+
+	/**
+	 * 
+	 */
+	private Part makeChin() {
+		//chin
+		Part chin = new Chin(
+				Color.BLACK, 
+				((Head)parts.get(0)).getxPos()+25, 	//x
+				((Head)parts.get(0)).getyPos()+100, 		//y
+				((Head)parts.get(0)).getWidth() / 5,  	//w
+				((Head)parts.get(0)).getHeight() / 4, 	//h
+				-0, 	//start angle
+				-180);	//arc angle
+		return chin;
+	}
+
+	/**
+	 * @return
+	 */
+	private Part makeHead() {
+		//head
+		Part head = new Head(
+				Color.PINK, 
+				10, 	//x
+				30, 	//y
+				180, 	//w
+				150);	//h
+		parts.add(head);
+		return head;
+	}
+
+	/**
+	 * @return
+	 */
+	private Part makeTooth() {
+		Part tooth = new Tooth(
+				Color.WHITE,
+				((Head)parts.get(0)).getxPos()+75,		//x position
+				((Head)parts.get(0)).getyPos()+75		//y position
+				);
+		return tooth;
 	}
 
 }
