23682003a5cca385b75850e959ceea14a7c8138a
bachmeb <bachmeb@mail.uc.edu>
2014-07-28 15:12:44 -0400

    make a triangle

---
+++ Java1/java1_Lesson10/src/TriangleClassDemo.java
@@ -0,0 +1,20 @@
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class TriangleClassDemo extends Applet {
+
+	public void paint(Graphics g){
+		this.drawTriangle(g, 80, 120, 100, 110);
+	}
+	
+	private void drawTriangle(Graphics g, int bottomX, int bottomY, int base, int height){
+		g.drawLine(bottomX, bottomY, bottomX+base, bottomY);
+		g.drawLine(bottomX+base, bottomY, bottomX+base/2, bottomY-height);
+		g.drawLine(bottomX+base/2, bottomY-height, bottomX, bottomY);
+	}
+	public TriangleClassDemo() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+}
