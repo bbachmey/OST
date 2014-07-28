7e3673def56f2a3a00e1eeb3a1219e5331668168
bachmeb <bachmeb@mail.uc.edu>
2014-07-28 15:33:58 -0400

    added java1_Project10

---
+++ java1_Project10/src/Construction.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.HeadlessException;
+
+
+public class Construction extends Applet {
+
+	public Construction() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+}
