3752e2cd24cd0c889bb408fdd53b686e5c36be3b
bachmeb <bachmeb@mail.uc.edu>
2014-07-29 14:30:14 -0400

    added javadoc added method demo

---
+++ Java1/java1_Lesson11/src/MethodDemo.java
@@ -0,0 +1,25 @@
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class MethodDemo extends Applet {
+
+	public void paint(Graphics g){
+		drawTriangle(g,80,120,100,110);
+		drawTriangle(g,125,140,60,70);
+	}
+	
+	private void drawTriangle(Graphics g, int bottomX, int bottomY, int base, int height){
+		int rightX = bottomX + base;
+		int topX = bottomX + base/2;
+		int topY = bottomY - height;
+		
+		//
+		g.drawLine(bottomX, bottomY, rightX, bottomY);
+		g.drawLine(rightX, bottomY, topX, topY);
+		g.drawLine(topX, topY, bottomX, bottomY);
+		
+	}
+
+}
