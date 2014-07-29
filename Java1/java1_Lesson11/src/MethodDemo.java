08d291f4cff45a4fc856abe66dac52a9592565af
bachmeb <bachmeb@mail.uc.edu>
2014-07-29 14:37:23 -0400

    updated the value of rightX in different scopes

--- Java1/java1_Lesson11/src/MethodDemo.java
+++ Java1/java1_Lesson11/src/MethodDemo.java
@@ -3,15 +3,24 @@ import java.applet.Applet;
 import java.awt.*;
 
 
+/**
+ * @author bbachmey
+ * This class is an applet with a method 
+ * that draws two triangles
+ */
 public class MethodDemo extends Applet {
 
 	public void paint(Graphics g){
-		drawTriangle(g,80,120,100,110);
-		drawTriangle(g,125,140,60,70);
+		int rightX = 42;
+        g.drawString("rightX before the method call is " + rightX, 5,170); 
+        drawTriangle(g, 80, 120, 100, 110);
+        drawTriangle(g, 125, 140, 60, 70);
+        g.drawString("rightX after the method call is " + rightX, 5,200);
 	}
 	
 	private void drawTriangle(Graphics g, int bottomX, int bottomY, int base, int height){
 		int rightX = bottomX + base;
+		g.drawString("rightX in the method is " + rightX, 5,185);
 		int topX = bottomX + base/2;
 		int topY = bottomY - height;
 		
