f57ca118785f1bd1961c89a13fe554b44cbc9c9d
bbachmey <bbachmey@gmail.com>
2014-07-29 16:13:17 -0400

    updated MethodDemo

--- Java1/java1_Lesson11/src/MethodDemo.java
+++ Java1/java1_Lesson11/src/MethodDemo.java
@@ -10,25 +10,30 @@ import java.awt.*;
  */
 public class MethodDemo extends Applet {
 
-	public void paint(Graphics g){
-		int rightX = 42;
-        g.drawString("rightX before the method call is " + rightX, 5,170); 
-        drawTriangle(g, 80, 120, 100, 110);
-        drawTriangle(g, 125, 140, 60, 70);
-        g.drawString("rightX after the method call is " + rightX, 5,200);
-	}
-	
-	private void drawTriangle(Graphics g, int bottomX, int bottomY, int base, int height){
-		int rightX = bottomX + base;
-		g.drawString("rightX in the method is " + rightX, 5,185);
-		int topX = bottomX + base/2;
-		int topY = bottomY - height;
-		
-		//
-		g.drawLine(bottomX, bottomY, rightX, bottomY);
-		g.drawLine(rightX, bottomY, topX, topY);
-		g.drawLine(topX, topY, bottomX, bottomY);
-		
-	}
+    public void paint(Graphics g) {
+        drawHouse(g, 10, 100, 70, 30);
+        drawHouse(g, 100, 50, 60, 20);
+    }
+      
+    private void drawTriangle(Graphics g, int bottomX, int bottomY, int base, int height){
+        int rightX = bottomX + base;
+        g.drawString("rightX in the method is " + rightX, 5,185);
+        int topX = bottomX + base/2;
+        int topY = bottomY - height;
+            
+        g.drawLine(bottomX, bottomY, rightX, bottomY);
+        g.drawLine(rightX, bottomY, topX, topY);
+        g.drawLine(topX, topY, bottomX, bottomY);
+    }
+        
+    private void drawHouse(Graphics g, int bottomX, int bottomY, int width, int height){
+        int rightX = bottomX + width;
+        int topX = bottomX + width/2;
+        int topY = bottomY - height;
+        int halfHeight = height/2;
+     
+        g.drawRect(bottomX, topY, width, height);
+        this.drawTriangle(g, bottomX, topY, width, halfHeight);
+    }
 
 }
