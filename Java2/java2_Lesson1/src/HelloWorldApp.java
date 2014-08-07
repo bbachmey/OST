71388128063ca627af01d3f37f6624954c0e4f33
bbachmey <bbachmey@gmail.com>
2014-08-07 15:57:36 -0400

    add HelloWorldApp

---
+++ Java2/java2_Lesson1/src/HelloWorldApp.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author bbachmey
+ *
+ */
+public class HelloWorldApp {
+
+	/**
+	 * 
+	 */
+	public HelloWorldApp() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
