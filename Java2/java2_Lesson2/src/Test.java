2fe6001af598ca92bc8580167591d243b4e5144c
bbachmey <bbachmey@gmail.com>
2014-08-08 15:53:14 -0400

    add java2_Project2

--- Java2/java2_Lesson2/src/Test.java
+++ Java2/java2_Lesson2/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
             
     public static void main(String[] args) {
-        int 4tryme;
-        int HowAboutThis&That;
+//        int 4tryme;
+//        int HowAboutThis&That;
     }
 }  
