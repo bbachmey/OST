b94a27363be6a200b8304555d325838911784b09
bbachmey <bbachmey@gmail.com>
2014-08-08 15:59:44 -0400

    add FrameTest

---
+++ Java2/java2_Project2/src/java2_Project2/FrameTest.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package java2_Project2;
+
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+/**
+ * @author bbachmey
+ *
+ */
+public class FrameTest extends Frame {
+	
+	
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public FrameTest() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public FrameTest(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @throws HeadlessException
+	 */
+	public FrameTest(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public FrameTest(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
