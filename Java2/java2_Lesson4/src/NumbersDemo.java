1c4d6ffc0d427efb6f9b7c95244b97ec4bbec090
bbachmey <bbachmey@gmail.com>
2014-08-12 17:36:10 -0400

    switch to DecimalFormat

--- Java2/java2_Lesson4/src/NumbersDemo.java
+++ Java2/java2_Lesson4/src/NumbersDemo.java
@@ -1,19 +1,35 @@
 import java.applet.Applet;
 import java.awt.*;
 import java.text.NumberFormat;
+import java.text.DecimalFormat;;
 
 
 public class NumbersDemo extends Applet {
 	
+	@Override
+	public void start(){
+		resize(300,200);
+		
+	}
+	
+	@Override
 	public void paint(Graphics g){
-		NumberFormat nf = NumberFormat.getInstance();
-		nf.setMaximumFractionDigits(2);
+
+		DecimalFormat myFormat1 = new DecimalFormat("###,###.##");
+		DecimalFormat myFormat2 = new DecimalFormat("###,###");
+		
+//		NumberFormat nf = NumberFormat.getInstance();
+//		nf.setMaximumFractionDigits(2);
 		double area;
 		double radius = 12;
 
 		g.drawString("Area of a circle = (radius)^2*Pi", 10, 20);
 		g.drawString("If radius = " + radius, 10, 40);
-		g.drawString("The circle's area = " + nf.format(Math.pow(radius, 2)*Math.PI), 10, 60);
+		//g.drawString("The circle's area = " + nf.format(Math.pow(radius, 2)*Math.PI), 10, 60);
+		g.drawString("The circle's area = " + myFormat1.format(Math.pow(radius, 2)*Math.PI), 10, 60);
+		g.drawString("An alternate formatting without decimals ", 10, 95);
+		g.drawString("The circle's area= " + myFormat2.format(Math.pow(radius, 2)*Math.PI), 10, 110);
+		
 		
 	}
 
