67c956849f73734fe25378e6fa386672fdc8d6a1
bbachmey <bbachmey@gmail.com>
2014-08-12 09:56:04 -0400

    add PrimitiveTest

---
+++ Java2/java2_Lesson3/src/java2_Lesson3/PrimitiveTest.java
@@ -0,0 +1,13 @@
+package java2_Lesson3;
+
+public class PrimitiveTest {
+		   
+   public static void main(String[] args) {
+     byte x = 344444;
+     short y = -32769;
+     int z = 2147483648;
+     long w = 9000000000000;
+     long v = 9E8;
+     }
+
+}
