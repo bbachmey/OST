979c3e447eb883adb8ce491c96d8ae22e263b240
bbachmey <bbachmey@gmail.com>
2014-08-12 15:46:32 -0400

    string manipulation

--- Java2/java2_Lesson4/src/DemoStrings.java
+++ Java2/java2_Lesson4/src/DemoStrings.java
@@ -6,19 +6,26 @@ public class DemoStrings extends Applet {
 
 	@Override
 	public void start(){
-		
+
 	}
-	
+
 	public void paint(Graphics g){
 		int y = 15;
 		String str = "Java is hot";
 		g.drawString(str, 10, y*1);
-		String modify = str.replace("v","bb").replace('o','u').replace("is", "the");
-		g.drawString(str.substring(0,7) + " still" + str.substring(7,11), 10, y*2);
-		g.drawString("But used to make: ", 10, y*4);
-		g.drawString(modify, 10, y*5);
-		
-        
+
+		String java = str.substring(0,6);
+		g.drawString("The method substring - from index 0 to 6 gives: " + java, 10, y*2);
+		int len = "Hot Java".length();
+		g.drawString("The length of the string HotJava is: " + len, 10, y*4);
+		String obj = new String("A String is an Object");
+		g.drawString(obj, 10, y*6);
+		String word = "Mississippi";
+		g.drawString(word,  10,  y*8);
+		g.drawString(word.replace('i', 'a'),10, y*9);
+		g.drawString(word.toUpperCase(), 10, y*10);
+		g.drawString("The original word is still: " +word, 10, y*12);
+
 	}
 
 }
