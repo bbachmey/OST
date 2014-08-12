ae7856dfd872f63e6dff58fbece39c0b4ddbdd37
bbachmey <bbachmey@gmail.com>
2014-08-12 10:02:10 -0400

    fix PrimitiveTest

--- Java2/java2_Lesson3/src/java2_Lesson3/PrimitiveTest.java
+++ Java2/java2_Lesson3/src/java2_Lesson3/PrimitiveTest.java
@@ -3,11 +3,11 @@ package java2_Lesson3;
 public class PrimitiveTest {
 		   
    public static void main(String[] args) {
-     byte x = 344444;
-     short y = -32769;
-     int z = 2147483648;
-     long w = 9000000000000;
-     long v = 9E8;
+       int x = 344444;
+       int y = -32769;
+       int z = 2147483647;
+       long w = 9000000000000L;
+       double v = 9E8;
      }
 
 }
