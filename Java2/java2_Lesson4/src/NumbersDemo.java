54ab93f488e0ac45d76b0a6c7064cc7ce2c73508
bbachmey <bbachmey@gmail.com>
2014-08-12 17:26:37 -0400

    format number

--- Java2/java2_Lesson4/src/NumbersDemo.java
+++ Java2/java2_Lesson4/src/NumbersDemo.java
@@ -1,16 +1,19 @@
 import java.applet.Applet;
 import java.awt.*;
+import java.text.NumberFormat;
 
 
 public class NumbersDemo extends Applet {
 	
 	public void paint(Graphics g){
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(2);
 		double area;
 		double radius = 12;
 
 		g.drawString("Area of a circle = (radius)^2*Pi", 10, 20);
 		g.drawString("If radius = " + radius, 10, 40);
-		g.drawString("The circle's area = " + Math.pow(radius, 2)*Math.PI, 10, 60);
+		g.drawString("The circle's area = " + nf.format(Math.pow(radius, 2)*Math.PI), 10, 60);
 		
 	}
 
