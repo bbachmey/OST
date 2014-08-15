a566df93146bcca10874eae9f43b5d4d018053df
bbachmey <bbachmey@gmail.com>
2014-08-15 12:53:45 -0400

    add java2_Lesson6

---
+++ Java2/java2_Lesson6/src/Test.java
@@ -0,0 +1,34 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int rem = 2;
+		int i = 21;
+		System.out.println("After declaring and setting to 21, i is " + i);
+		System.out.println();
+		
+		//divide 2 into 21 and get the remainder (modulo)
+		int j = i % rem;
+		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
+		
+		--i;
+		j = i % rem;
+		
+		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
+		
+		--i;
+		j = i % rem;
+		
+		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
+		
+		--i;
+		j = i % rem;
+		
+		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
+		
+		--i;
+		j = i % rem;
+		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);	       
+	} 
+	
+}
