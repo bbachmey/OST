808256c2e323eb189fc6b340dc07b3d1401e593f
bbachmey <bbachmey@gmail.com>
2014-08-15 13:02:46 -0400

    test operand types

--- Java2/java2_Lesson6/src/Test.java
+++ Java2/java2_Lesson6/src/Test.java
@@ -2,31 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-
-        int x;
-        
-        x = 4 + 3 * 2;
-        System.out.println("When x = 4 + 3 * 2 the answer is " + x);
-//        System.out.println();
-        
-        x = (4 + 3) * 2; 
-        System.out.println("When x = (4 + 3) * 2 the answer is " + x);
-//        System.out.println();
-        
-        x = 4 % 3 * 2; 
-        System.out.println("When x = 4 % 3 * 2  the answer is " + x);
-//        System.out.println();
-        
-        x = 4 % (3 * 2);
-        System.out.println("When x = 4 % (3 * 2) the answer is " + x);
-//        System.out.println();
-        
-        x = 6 + 3 * 4 / 2 - 9;
-        System.out.println("When x = 6 + 3 * 4 / 2 - 9 the answer is " + x);
-//        System.out.println();
-        
-        x = (6 + 3) * 4 / 2 - 9;
-        System.out.println("When x = (6 + 3) * 4 / 2 - 9 the answer is " + x);
+        int b = 1;
+        int c = 2;
+        float d = b/c;
+        System.out.println("For integers, " + b + "/" + c + " is " + b/c);
+        System.out.println("If we make the answer a float, we get " + d);
         
 	} 
 	
