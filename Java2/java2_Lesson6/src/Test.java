c4676c9d56547798e71299627d0c0a1d1215cb9b
bbachmey <bbachmey@gmail.com>
2014-08-15 13:01:19 -0400

    test precedence

--- Java2/java2_Lesson6/src/Test.java
+++ Java2/java2_Lesson6/src/Test.java
@@ -2,33 +2,32 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int rem = 3;
-		int i = 21;
-		System.out.println("After declaring and setting to 21, i is " + i);
-		System.out.println();
-		
-		//divide 2 into 21 and get the remainder (modulo)
-		int j = i % rem;
-		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
-		
-		--i;
-		j = i % rem;
-		
-		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
-		
-		--i;
-		j = i % rem;
-		
-		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
-		
-		--i;
-		j = i % rem;
-		
-		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);
-		
-		--i;
-		j = i % rem;
-		System.out.println("When i is " + i + " and j is set to \"i % " + rem + "\", the value of j is " + j);	       
+
+        int x;
+        
+        x = 4 + 3 * 2;
+        System.out.println("When x = 4 + 3 * 2 the answer is " + x);
+//        System.out.println();
+        
+        x = (4 + 3) * 2; 
+        System.out.println("When x = (4 + 3) * 2 the answer is " + x);
+//        System.out.println();
+        
+        x = 4 % 3 * 2; 
+        System.out.println("When x = 4 % 3 * 2  the answer is " + x);
+//        System.out.println();
+        
+        x = 4 % (3 * 2);
+        System.out.println("When x = 4 % (3 * 2) the answer is " + x);
+//        System.out.println();
+        
+        x = 6 + 3 * 4 / 2 - 9;
+        System.out.println("When x = 6 + 3 * 4 / 2 - 9 the answer is " + x);
+//        System.out.println();
+        
+        x = (6 + 3) * 4 / 2 - 9;
+        System.out.println("When x = (6 + 3) * 4 / 2 - 9 the answer is " + x);
+        
 	} 
 	
 }
