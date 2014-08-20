efc061ab59a409a418c07279c5f8d9cd8ca88e55
bbachmey <bbachmey@gmail.com>
2014-08-20 15:52:04 -0400

    update Calculation

--- Java2/java2_Lesson6/src/Calculation.java
+++ Java2/java2_Lesson6/src/Calculation.java
@@ -1,26 +1,26 @@
 import java.applet.Applet;
-import java.awt.*;
+import java.awt.Graphics;
 
 
 public class Calculation extends Applet {
-	
+
 	//make room for display
 	public void start(){
 		resize(300,200);
-		
+
 	}
 
 	public void paint(Graphics g){
 
-	     int diameter = 50;
-	        // int radius = 1/2 * diameter;
-	        double area;
+		double diameter = 50;
+		double radius = 1/2.0 * diameter;
+		double area;
 
-	        area = Math.PI * Math.pow(diameter/2,2); 
-			        
-		    g.drawOval(10,10,diameter/2, diameter/2);
-	        g.drawString("My circle's area is " + area, 10, 100);
+		area = Math.PI * Math.pow(radius,2); 
+
+		g.drawOval(10,10,(int)radius,(int)radius);
+		g.drawString("My circle's area is " + area, 10, 100);
 
 	}
-	
+
 }
