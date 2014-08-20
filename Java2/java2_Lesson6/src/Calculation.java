1bdea7558da6b52e85f9bd1887daff0a8b147253
bbachmey <bbachmey@gmail.com>
2014-08-20 16:14:35 -0400

    finish lesson 6

--- Java2/java2_Lesson6/src/Calculation.java
+++ Java2/java2_Lesson6/src/Calculation.java
@@ -1,6 +1,6 @@
 import java.applet.Applet;
 import java.awt.Graphics;
-
+import java.text.*;
 
 public class Calculation extends Applet {
 
@@ -11,20 +11,21 @@ public class Calculation extends Applet {
 	}
 
 	public void paint(Graphics g){
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(2);
 
-		double diameter = 50;
+		double diameter = 52;
 		double radius = 1/2.0 * diameter;
 		double area;
 
 		area = Math.PI * Math.pow(radius,2); 
 
 		g.drawOval(10,10,(int)radius,(int)radius);
-		g.drawString("My circle's area is " + area, 10, 100);
+		g.drawString("My circle's area is " + nf.format(area), 10, 100);
 
-
-		g.drawString("Answer is " + 1 + 2, 100, 120);
-
-		g.drawString("Answer is " +(1+2), 100, 130);
+		//		g.drawString("My circle's area is " + area, 10, 100);
+		//		g.drawString("Answer is " + 1 + 2, 100, 120);
+		//		g.drawString("Answer is " +(1+2), 100, 130);
 
 	}
 
