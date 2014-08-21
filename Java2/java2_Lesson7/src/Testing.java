19e0f1e0c7fc6521fc0b35abefdfc4fb2ca2085c
bbachmey <bbachmey@gmail.com>
2014-08-21 11:38:43 -0400

    edit Testing

--- Java2/java2_Lesson7/src/Testing.java
+++ Java2/java2_Lesson7/src/Testing.java
@@ -9,19 +9,43 @@ public class Testing {
        String grade;
        Scanner scan = new Scanner(System.in);
        
-       System.out.print("Enter a grade to test: ");
-       score = scan.nextInt();		
+//       System.out.print("Enter a grade to test: ");
+//       score = scan.nextInt();		
        
-       if (score >= 90) 
-           grade = "A";
-       else if (score >= 80) 
-           grade = "B";
-       else if (score >= 70) 
-           grade = "C";   
-       else if (score >= 60)
-           grade = "D"; 
-       else
-           grade = "F";
-       System.out.println("For a score of " + score + ", the grade is " + grade);
+//       if (score >= 90) 
+//           grade = "A";
+//       else if (score >= 80) 
+//           grade = "B";
+//       else if (score >= 70) 
+//           grade = "C";   
+//       else if (score >= 60)
+//           grade = "D"; 
+//       else
+//           grade = "F";
+       
+
+	   	 
+//       if (score < 60)  grade = "F"; else grade = "unknown";     
+//       if (score >= 60 && score < 70) grade = "D";
+//       if (score >= 70 && score < 80) grade = "C";
+//       if (score >= 80 && score < 90) grade = "B";
+//       if (score >= 90)  grade = "A";
+//       
+//       System.out.println("For a score of " + score + ", the grade is " + grade);
+       
+
+       int year;
+       boolean leap;
+
+
+       System.out.print("Enter a year to test: ");
+       year = scan.nextInt();		
+	   	 
+       leap = (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);
+
+       System.out.println("It is " + leap + " that " + year + " is a leap year.");       
+       
+       
+       
     }
 }
