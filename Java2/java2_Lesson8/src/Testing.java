2901b190ed82e411637af83d41e4e87710b00556
bbachmey <bbachmey@gmail.com>
2014-08-25 11:20:46 -0400

    test object equivalence

--- Java2/java2_Lesson8/src/Testing.java
+++ Java2/java2_Lesson8/src/Testing.java
@@ -2,15 +2,23 @@ public class Testing {
 
     public static void main(String[] args)  { 
     
-        int score = 85;
-        String grade = "?";		
-        
-        if (score < 80)  
-            { if (score < 70) 
-                grade = "C"; } 
-        else
-            grade = "B";
+//        int score = 85;
+//        String grade = "?";		
+//        
+//        if (score < 80)  
+//            { if (score < 70) 
+//                grade = "C"; } 
+//        else
+//            grade = "B";
+//
+//        System.out.println("For a score of " + score + ", the grade is " + grade);
+    	
 
-        System.out.println("For a score of " + score + ", the grade is " + grade);
+
+	    String stringLiteral = "are they the same";
+    	String stringObject = new String ("are they the same"); 
+    	System.out.println("stringLiteral == stringObject? " + (stringLiteral == stringObject));
+    	System.out.println("The sequence of characters is the same is " + stringLiteral.equals(stringObject));
+    	
     }
 }
