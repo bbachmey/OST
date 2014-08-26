b11281d61bd71dc8d6e4f705f995434a7a6c19b9
bbachmey <bbachmey@gmail.com>
2014-08-26 17:36:08 -0400

    add student grade test

--- Java2/java2_Lesson10/src/BeatleJuice.java
+++ Java2/java2_Lesson10/src/BeatleJuice.java
@@ -9,12 +9,16 @@ class Beatlejuice {
         beatles[3] = "Ringo Starr";
         beatles[4] = "George Martin";
         
-        System.out.println("Element at index 0: " + beatles[0]);
-        System.out.println("Element at index 1: " + beatles[1]);
-        System.out.println("Element at index 2: " + beatles[2]);
-        System.out.println("Element at index 3: " + beatles[3]);
-        System.out.println("Element at index 4: " + beatles[4]);
+//        System.out.println("Element at index 0: " + beatles[0]);
+//        System.out.println("Element at index 1: " + beatles[1]);
+//        System.out.println("Element at index 2: " + beatles[2]);
+//        System.out.println("Element at index 3: " + beatles[3]);
+//        System.out.println("Element at index 4: " + beatles[4]);
 
+        for (int i=0; i < beatles.length; i++){
+          System.out.println("Element at index " + i + ": " + beatles[i]);
+        }
+                
         System.out.println("\nSize of the beatles array is " + beatles.length);
     }
 } 
