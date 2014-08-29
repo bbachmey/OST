fc4b036f3a3e1723b2e21b6ebc44fd70455124fb
bbachmey <bbachmey@gmail.com>
2014-08-29 10:15:02 -0400

    test array values

--- Java2/java2_Lesson11/src/Test.java
+++ Java2/java2_Lesson11/src/Test.java
@@ -2,8 +2,30 @@
 class Test {
 
 	public static void main(String[] args) {
-		int[] testArray = new int[4];
-		System.out.println(testArray.getClass().getSuperclass());	
+		
+		int[] myIntArray = new int[4];
+		double [] myDoubleArray = new double[2];
+		char [] myCharArray = new char[2];
+		boolean [] myBooleanArray = new boolean[2];
+		String [] myStringArray = new String[6];
+		Object [] myObjectArray = new Object[5];
+		Thread [] myThreadArray = new Thread[2];
+
+		// this is what the null character looks like
+		char showMe = '\u0000';  
+		
+		// What does the first element in each look like?
+		System.out.println("The value of myIntArray[0] is " + myIntArray[0]);
+		System.out.println("The value of myDoubleArray[0] is " + myDoubleArray[0]);
+		System.out.println("The value of myCharArray[0] is " + myCharArray[0]);
+		System.out.println("The value of myBooleanArray[0] is " + myBooleanArray[0]);
+		System.out.println("The value of myStringArray[0] is " + myStringArray[0]);
+		System.out.println("The value of myObjectArray[0] is " + myObjectArray[0]);
+		System.out.println("The value of myThreadArray[0] is " + myThreadArray[0]);
+		
+		// to compare with the char array element
+		System.out.println("The value of showMe is " + showMe);  
+
 	}
-	
+
 } 
