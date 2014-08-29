cf3f6f7c71b8da7f9de327fa9cdb6b3694f2b601
bbachmey <bbachmey@gmail.com>
2014-08-29 09:35:36 -0400

    add java2_Lesson11

---
+++ Java2/java2_Lesson11/src/ClickableBox.java
@@ -0,0 +1,10 @@
+import java.awt.event.MouseAdapter;
+
+
+public class ClickableBox extends MouseAdapter {
+
+	public ClickableBox() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Java2/java2_Quiz10/src/Quiz.java
@@ -0,0 +1,12 @@
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for ( int c = 1 ; c != 20 ; c=c+2 )
+			  System.out.println("What's happening!");
+
+
+	}
+
+}
