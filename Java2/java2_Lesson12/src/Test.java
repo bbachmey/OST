9f6170c850f8ac575b13c123837a343cfd128ff9
bbachmey <bbachmey@gmail.com>
2014-09-05 14:20:45 -0400

    update Test with grading scale

--- Java2/java2_Lesson12/src/Test.java
+++ Java2/java2_Lesson12/src/Test.java
@@ -21,40 +21,56 @@ class Test {
 //    }
 	
 	  
-	    int total;
-	                                                                    
-	    public static void main(String[] args){
-	        Test myTest = new Test();     // instantiate the class Test
-	        myTest.demo();                      // start the class
-	    }
-	 
-	    public void demo(){
-	        int[] table = new int[12];          // instantiate the array table
-	        for (int i=0; i < table.length; i++)
-	        {   
-	            table[i] = 1;                   // populate the array table with all 1's
-	            System.out.print(table[i] + " ");
-	        }
-
-	        int[] table2 = new int[12];
-	        System.arraycopy(table, 0, table2, 0, table.length);
-	        total = sum(table2);                // call the method sum and pass the copy of array table
-	        
-	        System.out.println("The total is " + total);
-	        System.out.println("After method invocation completes \n and control has returned, values are");
-	        for (int i=0; i < table.length; i++) 
-	            System.out.print(table[i] + " ");
-	    }
-
-	    public int sum(int [] array) {
-	        total = 0;
-	        for (int s=0; s < array.length; s++)    // when the index s equals the length, loop will stop
-	        {
-	           total = total + array[s];               // find the sum of all of the elements in the array
-	           array[s]= array[s] + 1;
-	        } 
-	        return total;
-	    }
+//	    int total;
+//	                                                                    
+//	    public static void main(String[] args){
+//	        Test myTest = new Test();     // instantiate the class Test
+//	        myTest.demo();                      // start the class
+//	    }
+//	 
+//	    public void demo(){
+//	        int[] table = new int[12];          // instantiate the array table
+//	        for (int i=0; i < table.length; i++)
+//	        {   
+//	            table[i] = 1;                   // populate the array table with all 1's
+//	            System.out.print(table[i] + " ");
+//	        }
+//
+//	        int[] table2 = new int[12];
+//	        System.arraycopy(table, 0, table2, 0, table.length);
+//	        total = sum(table2);                // call the method sum and pass the copy of array table
+//	        
+//	        System.out.println("The total is " + total);
+//	        System.out.println("After method invocation completes \n and control has returned, values are");
+//	        for (int i=0; i < table.length; i++) 
+//	            System.out.print(table[i] + " ");
+//	    }
+//
+//	    public int sum(int [] array) {
+//	        total = 0;
+//	        for (int s=0; s < array.length; s++)    // when the index s equals the length, loop will stop
+//	        {
+//	           total = total + array[s];               // find the sum of all of the elements in the array
+//	           array[s]= array[s] + 1;
+//	        } 
+//	        return total;
+//	    }
 	
+	 
+	public static void main(String[] args){
+	    Test myTest = new Test();           
+	    myTest.demo();                      
+	}
+	 
+	public void demo(){
+	 	String [] grades = new String[4];
+	    defaultFillGrade(grades);
+	    for (int i=0; i < grades.length; i++) System.out.print(grades[i] + " ");
+	}
+
+	public void defaultFillGrade(String [] grades){
+	    for (int studentNum = 0; studentNum < grades.length; studentNum++) 
+	    	grades[studentNum] = "A";
+	} 
 	
 } 
