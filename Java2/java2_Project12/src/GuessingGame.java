5ef29761e7a8018a0f5701a7df4549bf7d4bb8fe
bbachmey <bbachmey@gmail.com>
2014-09-08 13:11:43 -0400

    add code changes from project instructions

--- Java2/java2_Project12/src/GuessingGame.java
+++ Java2/java2_Project12/src/GuessingGame.java
@@ -17,65 +17,92 @@ public class GuessingGame extends Applet {
 	private MaskableBox boxes[][];
 	private Color boxColors[][];
 
-    private Button resetButton;
-  
-    public void init() {
-        boxes = new MaskableBox[ROWS][COLS];
-        boxColors = new Color[ROWS][COLS];
-        resetButton = new Button("Reset Colors");
-        resetButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                randomizeColors();
-                buildBoxes();
-                repaint();
-            }
-        });
-        add(resetButton);
-        //separate building colors so we can add a button later
-        //to re-randomize them.
-        randomizeColors();
-        buildBoxes();
-    }
+	private Button resetButton;
+
+	//this is used to keep track of boxes that have been matched.
+	private boolean matchedBoxes[][];
+	//this is used to keep track of two boxes that have been clicked.
+	private MaskableBox chosenBoxes[];
 
 
-    private void removeMouseListeners() {
-        for(int row = 0; row < boxes.length; row ++) {
-            for(int col = 0; col < boxes[row].length; col++) {
-                removeMouseListener(boxes[row][col]);
-            }
-        }
-    }
-  
-    public void paint(Graphics g) {
-        for(int row = 0; row < boxes.length; row ++) {
-            for(int col = 0; col < boxes[row].length; col++) {
-                if(boxes[row][col].isClicked()) {
-                    boxes[row][col].setMaskColor(Color.black);
-                    boxes[row][col].setMask(!boxes[row][col].isMask());
-                    boxes[row][col].setClicked(false);
-                }
-                boxes[row][col].draw(g);
-            }
-        }
-    }
-  
-    private void buildBoxes() {
-        removeMouseListeners();
-        for(int row = 0; row < boxes.length; row++) {
-            for(int col = 0; col < boxes[row].length; col++) {
-                boxes[row][col] = 
-                  new MaskableBox(START_X + col * BOX_WIDTH,
-                                  START_Y + row * BOX_HEIGHT,
-                                  BOX_WIDTH,
-                                  BOX_HEIGHT,
-                                  Color.gray,
-                                  boxColors[row][col],
-                                  true,
-                                  this);
-                addMouseListener(boxes[row][col]);
-            }
-        }
-    }
+
+	public void init() {
+		boxes = new MaskableBox[ROWS][COLS];
+		boxColors = new Color[ROWS][COLS];
+		resetButton = new Button("Reset Colors");
+		resetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				randomizeColors();
+				buildBoxes();
+				repaint();
+			}
+		});
+		add(resetButton);
+		//separate building colors so we can add a button later
+		//to re-randomize them.
+		randomizeColors();
+		buildBoxes();
+	}
+
+
+	private void removeMouseListeners() {
+		for(int row = 0; row < boxes.length; row ++) {
+			for(int col = 0; col < boxes[row].length; col++) {
+				removeMouseListener(boxes[row][col]);
+			}
+		}
+	}
+
+	public void paint(Graphics g) {
+		// loop through the boxes rows
+		for (int row = 0; row < boxes.length; row++) {
+			// loop through each rows columns.
+			for (int col = 0; col < boxes[row].length; col++) {
+				// if we are at a clicked box.
+				if (boxes[row][col].isClicked()) {
+					// un click it
+					boxes[row][col].setClicked(false);
+					//only do game logic on boxes that haven't been matched.
+					if (!matchedBoxes[row][col]) {
+						gameLogic(boxes[row][col]);
+					}
+				}
+			}
+		}
+		//loop through the boxes and draw them.
+		for (int row = 0; row < boxes.length; row++) {
+			for (int col = 0; col < boxes[row].length; col++) {
+				boxes[row][col].draw(g);
+			}
+		}
+	}
+
+	private void buildBoxes() {
+		// need to clear any chosen boxes when building new array.
+		chosenBoxes = new MaskableBox[2];
+		// create a new matchedBoxes array
+		matchedBoxes = new boolean [ROWS][COLS];
+
+		removeMouseListeners();
+		for(int row = 0; row < boxes.length; row++) {
+			for(int col = 0; col < boxes[row].length; col++) {
+				boxes[row][col] = 
+						new MaskableBox(START_X + col * BOX_WIDTH,
+								START_Y + row * BOX_HEIGHT,
+								BOX_WIDTH,
+								BOX_HEIGHT,
+								Color.gray,
+								boxColors[row][col],
+								true,
+								this);
+				addMouseListener(boxes[row][col]);
+			}
+		}
+	}
+
+	public void gameLogic(MaskableBox box){
+
+	}
 
 	private void randomizeColors() {
 		int[] chosenColors = {0, 0, 0, 0, 0, 0, 0, 0 };
