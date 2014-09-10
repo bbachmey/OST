d6d2daa43be8c2288132687f9fdd8ae4eacc064c
bbachmey <bbachmey@gmail.com>
2014-09-10 14:43:26 -0400

    change initial condition of Test

--- Java2/java2_Lesson14/src/Test.java
+++ Java2/java2_Lesson14/src/Test.java
@@ -1,12 +1,13 @@
 class Test {  
 
     public static void main(String[] args){
-        int n = 0;
-        for (int i = 0; i < 5; i++)
-            for (int k = i ; k < 5-i; k++)
-            {
-                n++;
-                System.out.println(n + ": i is " + i + " and k is " + k);
-            }  
+       int n = 0;
+       for (int i = 5; i < 0; i--)
+           for (int k = 0 ; k < 1; k++)
+           {
+               System.out.print("here I am again: ");
+               n++;
+               System.out.println(n + ": i is " + i + " and k is " + k);
+           }  
     }
-}
+}             
