678c0fa95af09f24713356302194400ae8ecbfa5
bbachmey <bbachmey@gmail.com>
2014-09-10 14:41:10 -0400

    trace Test

--- Java2/java2_Lesson14/src/Test.java
+++ Java2/java2_Lesson14/src/Test.java
@@ -1,8 +1,14 @@
 class Test {  
 
     public static void main(String[] args){
+        int n=0;
+    
         for (int i = 0; i < 4; i++)
             for (int k = 0 ; k < 5; k++)
-                System.out.println("here I am again");
+            {
+                System.out.print("here I am again: ");
+                n++;
+                System.out.println(n + ": i is " + i + " and k is now " + k);
+            }  
     }
 }  
