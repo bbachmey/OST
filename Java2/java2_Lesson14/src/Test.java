01c582fe52423c56656724d37b69cc3882c16f9c
bbachmey <bbachmey@gmail.com>
2014-09-10 14:41:46 -0400

    retrace Test

--- Java2/java2_Lesson14/src/Test.java
+++ Java2/java2_Lesson14/src/Test.java
@@ -2,13 +2,12 @@ class Test {
 
     public static void main(String[] args){
         int n=0;
-    
-        for (int i = 0; i < 4; i++)
-            for (int k = 0 ; k < 5; k++)
+  
+        for (int i = 0; i < 6; i++)
+            for (int k = 0 ; k < 1; k++)
             {
                 System.out.print("here I am again: ");
                 n++;
-                System.out.println(n + ": i is " + i + " and k is now " + k);
-            }  
-    }
+                System.out.println(n + ": i is " + i + " and k is now " + k);            }  
+            }
 }  
