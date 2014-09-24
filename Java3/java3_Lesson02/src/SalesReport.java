097ff4dea1894c3d40749faa0b3900afe16ca2b8
bbachmey <bbachmey@gmail.com>
2014-09-24 15:24:39 -0400

    add who to findMax

--- Java3/java3_Lesson02/src/SalesReport.java
+++ Java3/java3_Lesson02/src/SalesReport.java
@@ -27,12 +27,15 @@ public class SalesReport {
                 
     private void findMax() {
     	int max = 0;
-    	int i;
-    	for (i=0; i < sales.length; i++)
+    	int who = 0;
+    	for (int i=0; i < sales.length; i++)
     	{
-    		if (max < sales[i]) max=sales[i];
+    		if (max < sales[i]){
+    			max=sales[i];
+    			who = i;
+    		}
     	}
-    	System.out.println("\nSalesperson " + (i+1) + " had the highest sales with $" + max);
+    	System.out.println("\nSalesperson " + (who+1) + " had the highest sales with $" + max);
 		
 	}
 
