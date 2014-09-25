102baa391e8ffbd61afdecdb0ef983665989caeb
bbachmey <bbachmey@gmail.com>
2014-09-25 11:29:10 -0400

    add TestDate

---
+++ Java3/java3_Lesson03/src/time/TestDate.java
@@ -0,0 +1,13 @@
+package time;
+
+import java.sql.*;
+import java.util.*;
+
+public class TestDate {
+
+    public static void main(String[ ] args){
+    	java.util.Date myDate = new java.util.Date();
+        System.out.println(myDate.toString());
+    }
+    
+}
