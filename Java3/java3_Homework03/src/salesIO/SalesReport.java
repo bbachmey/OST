a15ab238c931714f6afa111a6082b029f247e791
bbachmey <bbachmey@gmail.com>
2014-09-26 14:28:47 -0400

    finish java3_Homework03

--- Java3/java3_Homework03/src/salesIO/SalesReport.java
+++ Java3/java3_Homework03/src/salesIO/SalesReport.java
@@ -1,111 +1,122 @@
 package salesIO;
 import java.util.Scanner;
 
+import utilities.*;
+
 public class SalesReport {
-	int salespeople;
-	int sum;
-	double avg;
-	int sales[];
-	Scanner scan = new Scanner(System.in);
-	int guess;
+	private Scanner scanner = new Scanner(System.in);
+	private SalesComputations computer;
+
+	private int salespeople;
+	private int sales[][];//[rows][columns]
+	private int sum;
+	private double avg;
+	private int max;
+	private int min;
+	private int more[];
+	private int guess;
+	private int rows;
+
+	public static final int COLUMNS = 3;
+	public static final int ID_COLUMN = 0;
+	public static final int NUMBER_COLUMN = 1;
+	public static final int TOTAL_COLUMN = 2;	
 
 	public SalesReport(){
+		//empty constructor
+	}
+
+	public void init(){
+		input();
+		calculate();
+		output();
+
+		inputAgain();
+		calculateAgain();
+		outputAgain();
+
+	}
+
+
+
+	private void input() {
+		//ask for a number
 		System.out.print("Enter the number of salespersons: ");
-		this.salespeople = scan.nextInt(); //REMOVE the 3!!!
-		this.sales = new int[salespeople];
-	}
-
-	public SalesReport(int howMany) {
-		// TODO Auto-generated constructor stub
-		this.salespeople = howMany;
-		this.sales = new int[salespeople];
-	}
-
-	public void testMe(){
-		getSalesInput();
-		provideSalesOutput();
-		findMax();
-		findMin();
-		getAnInt();
-		findMore();
-	}
-
-	private void getAnInt() {
-
-
-			System.out.print("\nEnter a number: ");
-			guess = scan.nextInt();
-
-		
-	}
-
-	private void findMax() {
-		int max = sales[0];
-		int who = 0;
-		for (int i=0; i < sales.length; i++)
-		{
-			if (max < sales[i]){
-				max=sales[i];
-				who = i;
-			}
-		}
-		System.out.println("\nSalesperson " + (who+1) + " had the highest sales with $" + max);
-
-	}
-
-	private void findMin() {
-		int min = sales[0];
-		int who = 0;
-		for (int i=0; i < sales.length; i++)
-		{
-			if (sales[i] < min){
-				min=sales[i];
-				who = i;
-			}
-		}
-		System.out.println("\nSalesperson " + (who+1) + " had the lowest sales with $" + min);
-
-	}
-
-	private void findMore() {
-		int people = 0;
-
-		for (int i=0; i < sales.length; i++)
-		{
-			if (sales[i] > guess){
-				System.out.println("\nSalesperson " + (i+1) + " sold more than " + guess + " with sales of " + sales[i] );
-				people++;
-			}
-		}
-		System.out.println("\nThe total number of salespeople whose sales exceeded the value entered is: " + people);
-
-	}
-
-	public void getSalesInput(){
-		//        Scanner scan = new Scanner(System.in);
-
+		//store the number given
+		salespeople = scanner.nextInt();
+		//set the number of rows equal to the number of salespeople
+		rows = salespeople;
+		//instantiate a 2d array 
+		//[rows][columns]
+		sales = new int[rows][COLUMNS];
+		//loop through the sales array and ask for sales amounts
 		for (int i=0; i<sales.length; i++){
-			System.out.print("Enter sales for salesperson " + (i+1) + ": ");
-			sales[i] = scan.nextInt();
+			int number = i+1;
+			System.out.print("Enter sales for salesperson number " + number + ": ");
+			//wait for the user response
+			int amount = scanner.nextInt();
+			//populate each column in the row
+			sales[i][0] = i;
+			sales[i][1] = number;
+			sales[i][2] = amount;
 		}
+
 	}
 
-	public void provideSalesOutput(){
-		System.out.println("\nSalesperson   Sales");
+	private void calculate() {
+		computer = new SalesComputations();
+		sum = computer.findSum(sales);
+		avg = computer.findAvg(salespeople,sum);
+		max = computer.findMax(sales);
+		min = computer.findMin(sales);
+
+	}
+
+	public void output(){
+		//List all sales
+		System.out.println("Salesperson   Sales");
 		System.out.println("--------------------");
-		sum = 0;
 		for (int i=0; i<sales.length; i++){
-			System.out.println("     " + (i+1) + "         " + sales[i]);
-			sum = sum + sales[i];
+			System.out.println("     " + (sales[i][NUMBER_COLUMN]) + "         " + sales[i][TOTAL_COLUMN]);
 		}
-		System.out.println("\nTotal sales: " + sum);
-		//Homework 01
-		System.out.println("\nAverage sales: " + computeAverage(sum,salespeople));
+		//New line
+		System.out.println("\n");
+		//Total
+		System.out.println("Total sales: " + sum);
+		//Average
+		System.out.println("Average sales: " + avg);
+		//Highest
+		System.out.println("Salesperson " + (sales[max][NUMBER_COLUMN]) + " had the highest sales with $" + sales[max][TOTAL_COLUMN]);
+		//Lowest
+		System.out.println("Salesperson " + (sales[min][NUMBER_COLUMN]) + " had the lowest sales with $" + sales[min][TOTAL_COLUMN]);
+
 	}
 
-	public int computeAverage(int s, int p){
-		avg = s/p;
+	private void inputAgain() {
+		System.out.print("\nEnter a $ amount: ");
+		guess = scanner.nextInt();
 
-		return (int)avg;
+	}
+
+	private void calculateAgain() {
+		//get an array of IDs 
+		more = computer.findMore(sales, guess);
+
+	}
+
+	private void outputAgain() {
+
+		if (more.length==0){
+			System.out.println("No salespeople sold more than $" + guess );
+		}
+		else{
+
+			for (int i=0; i<more.length; i++){
+				int id = more[i];
+				System.out.println("Salesperson number " + sales[id][NUMBER_COLUMN] + " sold more than $" + guess + " with sales of $" + sales[id][TOTAL_COLUMN] );
+			}
+			
+			System.out.println("The total number of salespeople whose sales exceeded the $ amount entered is: " + more.length ); 
+		}
 	}
 } 
--- Java3/java3_Homework03/src/utilities/SalesComputations.java
+++ Java3/java3_Homework03/src/utilities/SalesComputations.java
@@ -1,5 +1,71 @@
 package utilities;
 
+import java.util.Scanner;
+
+import salesIO.SalesReport;
+
 public class SalesComputations {
+
+
+	public int findMax(int sales[][]) {
+		int max = sales[0][SalesReport.TOTAL_COLUMN];
+		int who = sales[0][SalesReport.ID_COLUMN];
+		for (int i=0; i < sales.length; i++)
+		{
+			if (max < sales[i][SalesReport.TOTAL_COLUMN]){
+				who=sales[i][SalesReport.ID_COLUMN]; //ID
+			}
+		}
+		return who; 
+
+	}
+
+	public int findMin(int sales[][]) {
+		int min = sales[0][SalesReport.TOTAL_COLUMN];
+		int who = sales[0][SalesReport.ID_COLUMN];
+		for (int i=0; i < sales.length; i++)
+		{
+			if (sales[i][SalesReport.TOTAL_COLUMN] < min){
+				who=sales[i][SalesReport.ID_COLUMN];
+			}
+		}
+		return who;
+
+	}
+
+	public int[] findMore(int sales[][], int guess) {
+		int more[];
+		int many = 0;
+		int counter = 0;
+		for (int i=0; i < sales.length; i++)
+		{
+			if (sales[i][SalesReport.TOTAL_COLUMN] > guess){
+				many++;
+			}
+		}
+		more = new int[many];
+		for (int i=0; i < sales.length; i++)
+		{
+			if (sales[i][SalesReport.TOTAL_COLUMN] > guess){
+				more[counter]=sales[i][SalesReport.ID_COLUMN];
+				counter++;
+			}
+		}
+		return more;
+		
+	}
+
+	public int findSum(int sales[][]) {
+		int sum = 0;
+		for (int i=0; i<sales.length; i++){
+			sum = sum + sales[i][SalesReport.TOTAL_COLUMN];
+		}
+		return sum;
+	}
+
+	public int findAvg(int people, int sum) {
+		double average = sum/people;
+		return (int)average;
+	}
 
 }
