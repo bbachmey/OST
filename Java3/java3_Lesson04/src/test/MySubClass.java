767c6800d512ace4f23d58b4184697a333af8348
bbachmey <bbachmey@gmail.com>
2014-10-01 13:58:51 -0400

    shadow i

--- Java3/java3_Lesson04/src/test/MySubClass.java
+++ Java3/java3_Lesson04/src/test/MySubClass.java
@@ -2,12 +2,22 @@ package test;
 
 public class MySubClass extends MySuperClass {
 	
+	int i = 42;
+	
 	public static void main(String[] args){
 		
 		MySubClass testing = new MySubClass();
-		System.out.println("From MySubClass, the value of testing.i is " + testing.i);
-		//System.out.println("Notice how this will not work " + testing.super.i);
+		//System.out.println("From MySubClass, the value of testing.i is " + testing.i);
 		
+		testing.whatsHere();
+		
+		//System.out.println("Notice how this will not work " + testing.super.i);
+	    
 	}
+
+    public void whatsHere() {
+        System.out.println("From MySubClass, this.i is " + this.i + " and i is " + i);
+        System.out.println(" shadowing MySuperClass's value (super.i): " + super.i);
+    }
 
 }
