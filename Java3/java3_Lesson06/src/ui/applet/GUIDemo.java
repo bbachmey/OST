74bfea473d3958e97dbbf1ca63cdb12984fa8d12
bbachmey <bbachmey@gmail.com>
2014-10-02 11:24:09 -0400

    add ActionPanel to GUIDemo

--- Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
+++ Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
@@ -1,22 +1,17 @@
 package ui.applet;
 
 import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
+import ui.panels.ActionPanel;
 
-public class GUIDemo extends Applet implements ItemListener {
-    private final String DRAW = "Draw";
-    Checkbox draw;
-	
-    public void init(){
-        draw = new Checkbox(DRAW);
-        add(draw);
-        draw.addItemListener(this);
-    }
-	
-    public void itemStateChanged(ItemEvent e){
-        System.out.println("I see you now!");
-        if (e.getSource() == draw)
-            System.out.println("I know you clicked " + e.getItem().toString());
-    }
+    public class GUIDemo extends Applet{
+        
+        
+        ActionPanel actionPanel;
+    
+        public void init(){
+            resize(600,400);
+            actionPanel = new ActionPanel();
+            add(actionPanel); 
+        }
+         
 }
