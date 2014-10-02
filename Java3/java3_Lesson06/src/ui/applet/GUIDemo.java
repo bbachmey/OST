a9b8287c190f117a5264263efa866984f28af380
bbachmey <bbachmey@gmail.com>
2014-10-02 11:28:07 -0400

    remove ActionPanel from GUIDemo

--- Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
+++ Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
@@ -1,17 +1,23 @@
 package ui.applet;
 
+import interfaces.Resettable;
 import java.applet.Applet;
-import ui.panels.ActionPanel;
+//import ui.panels.ActionPanel;
+import ui.panels.MainPanel;
 
-    public class GUIDemo extends Applet{
-        
-        
-        ActionPanel actionPanel;
+public class GUIDemo extends Applet implements Resettable{
+    //ActionPanel actionPanel;
+    MainPanel mainPanel;
+  
+    public void init() {
+        resize(600,400);
+        //actionPanel = new ActionPanel();
+        //add(actionPanel);
+        mainPanel = new MainPanel();
+        add(mainPanel);
+    }
     
-        public void init(){
-            resize(600,400);
-            actionPanel = new ActionPanel();
-            add(actionPanel); 
-        }
-         
+    public void resetComponents(){
+        mainPanel.resetComponents();
+    }
 }
