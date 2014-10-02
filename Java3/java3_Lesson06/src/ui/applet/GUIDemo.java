b37139fbe9db07bef060b1adff5bce7d4ba9184b
bbachmey <bbachmey@gmail.com>
2014-10-02 11:14:07 -0400

    add ActionListener to GUIDemo

--- Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
+++ Java3/java3_Lesson06/src/ui/applet/GUIDemo.java
@@ -1,13 +1,22 @@
 package ui.applet;
+
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GUIDemo extends Applet {
-
+public class GUIDemo extends Applet implements ItemListener {
     private final String DRAW = "Draw";
-    
+    Checkbox draw;
+	
     public void init(){
-        Checkbox draw = new Checkbox(DRAW);
-        add(draw);		                       
+        draw = new Checkbox(DRAW);
+        add(draw);
+        draw.addItemListener(this);
+    }
+	
+    public void itemStateChanged(ItemEvent e){
+        System.out.println("I see you now!");
+        if (e.getSource() == draw)
+            System.out.println("I know you clicked " + e.getItem().toString());
     }
 }
