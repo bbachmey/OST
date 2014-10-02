63f02a11cfa0ad9978e8d927085b67753aa17633
bbachmey <bbachmey@gmail.com>
2014-10-02 09:54:03 -0400

    add java3_Homework05

---
+++ Java3/java3_Homework05/src/shapes/Rectangle.java
@@ -0,0 +1,50 @@
+package shapes;
+
+public class Rectangle extends Shape {
+
+	int xPos;
+	int yPos;
+	int height;
+	int width;
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+
+}
