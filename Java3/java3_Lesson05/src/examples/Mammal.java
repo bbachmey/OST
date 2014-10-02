403ef82a1d48e7b41c18b6a6da362d7e63c58d37
bbachmey <bbachmey@gmail.com>
2014-10-02 09:12:45 -0400

    Cannot instantiate the type Mammal

--- Java3/java3_Lesson05/src/examples/Mammal.java
+++ Java3/java3_Lesson05/src/examples/Mammal.java
@@ -1,7 +1,10 @@
 package examples;
 
-public abstract class Mammal {
-	
-	public abstract void move();
+public abstract  class Mammal {  // an abstract class
 
+	public void move(){}         // no abstract methods
+    
+	public static void main(String [] args){
+		Mammal aMammal = new Mammal();  // try to instantiate
+	}	
 }
