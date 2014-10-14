91e5416e3f7eab9142289d46080c7f4e9eacbf0d
bbachmey <bbachmey@gmail.com>
2014-10-14 15:57:35 -0400

    update homework 7 with changes from homework 6

--- Java3/Java3_Homework07/src/shapes/Line.java
+++ Java3/Java3_Homework07/src/shapes/Line.java
@@ -1,16 +1,30 @@
 package shapes;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Line extends Shape {
 
 	int x2;
 	int y2;
 
+	public Line(int x, int y, Color lineColor) {
+		super(x, y, lineColor);
+		// TODO Auto-generated constructor stub
+	}
+	
 	@Override
-	public void draw() {
+	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public boolean containsLocation(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 	public int getX2() {
 		return x2;
 	}
@@ -26,6 +40,7 @@ public class Line extends Shape {
 	public void setY2(int y2) {
 		this.y2 = y2;
 	}
+
 
 
 	
