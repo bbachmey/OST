b8720c451c2044074b2938e26701a8ff98df54e6
bbachmey <bbachmey@gmail.com>
2014-11-05 15:57:05 -0500

    add Sorts

--- Java3/java3_Lesson09/src/utilities/TestRectangleSort.java
+++ Java3/java3_Lesson09/src/utilities/TestRectangleSort.java
@@ -9,19 +9,25 @@ public class TestRectangleSort {
 	}
 	
 	public void sortRectangles(){
+		// we create an array of Rectangles 
 		Rectangle[] figures = new Rectangle[3];
 		
+		// fill it with three Rectangle objects 
 		figures[0] = new Rectangle(60,30,160,100);
 		figures[1] = new Rectangle(10,120,40,150);
 		figures[2] = new Rectangle(90,125,143,163);
 		
+		// and then display their values, in order
 		System.out.println("Before shellSort:");
 		for (int i=0; i < figures.length; i++){
 			System.out.println("Area is " + figures[i].getArea() );
 		}
 		
+		// Once that's done, we call the Sortable.shellSort() method, 
+		//passing the array and figures to the method. 
 		Sortable.shellSort(figures);
 		
+		// Then we display the array in order again
 		System.out.println("\nAfter shellSort:");
 		for (int i = 0; i < figures.length; i++){
 			System.out.println("Area is " + figures[i].getArea());
