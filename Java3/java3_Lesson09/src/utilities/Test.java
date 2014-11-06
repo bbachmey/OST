f7e3943a4107e850a4f0fc2d152d72d928716c55
bbachmey <bbachmey@gmail.com>
2014-11-06 12:07:06 -0500

    cast Sortable objects back to Rectangles

--- Java3/java3_Lesson09/src/utilities/Test.java
+++ Java3/java3_Lesson09/src/utilities/Test.java
@@ -10,18 +10,30 @@ public class Test {
 	public void tryTypes(){
 		Rectangle rect1 = new Rectangle(60, 30, 260, 100);
 		Sortable rect2 = new Rectangle(10,120,40,150);
-		Sortable [] figures = {rect1, rect2};
+		Rectangle rect3 = (Rectangle)rect2;
+		Sortable [] figures = {rect1, rect3};
 		
 		System.out.println("rect1 Area: " + rect1.getArea() );
 		System.out.println("rect1 Upper Left: (" + rect1.uLX + "," + rect1.uLY + ")");
+		System.out.println("rect3 Area: " + rect3.getArea());
+        System.out.println("rect3 Upper Left: (" + rect3.uLX + ","+ rect3.uLY + ")");
 		
-		int compareTest1 = rect1.compareTo(rect2);
-		int compareTest2 = rect2.compareTo(rect1);
+		int compareTest1 = rect1.compareTo(rect3);
+		int compareTest2 = rect3.compareTo(rect1);
+
+        for (int i = 0; i < figures.length; i++) {
+        	//cast the item in the figures array from a Sortable object
+        	//back to a Rectangle object
+        	//rect1 and rect3 are still rectangles, even though they "are-a" Sortable object too
+        	Rectangle current = (Rectangle)figures[i];
+        	System.out.println("Area is " + current.getArea());
+        }
+            
 		if (compareTest1 > compareTest2){
-			System.out.println("rect1 is bigger than rect2");
+			System.out.println("rect1 is bigger than rect3");
 		}
 		else{
-			System.out.println("rect2 is bigger than rect1");
+			System.out.println("rect3 is bigger than rect1");
 		}
 	}
 }
