fa4ab16f381d69d5d64b9cd7a2f64b3b2da2a586
bbachmey <bbachmey@gmail.com>
2014-11-10 12:37:18 -0500

    add TestVectors

---
+++ Java3/java3_Lesson10/src/utilities/TestVectors.java
@@ -0,0 +1,53 @@
+package utilities;
+
+import java.awt.*;
+import java.util.*;
+
+public class TestVectors {
+
+	public static void main(String[] args){
+		TestVectors testMe = new TestVectors();
+		testMe.tryVectors();
+	}
+	
+	public void tryVectors(){
+		
+		Rectangle rect1 = new Rectangle(60,30,160,100);
+		Sortable rect2 = new Rectangle(10,120,40,150);
+		
+		Rectangle rect3 = (Rectangle)rect2;
+		
+		Oval oval1 = new Oval(60,30,160,100);
+		Sortable oval2 = new Oval(20,120,40,150);
+		
+		Oval oval3 = (Oval)oval2;
+		// When we made rect3 and oval3, we did not make new objects. 
+		//Instead, we cast an existing object to be seen differently from how it was declared earlier. 
+		//They do point to the same place though, so be careful making changes to either.
+		Point myPoint = new Point(55,55);
+		
+		//The Vector named moreFigures is created to hold 2 objects initially. 
+		Vector moreFigures = new Vector(2);
+
+		// As we add objects to the vector, it grows automatically to accommodate more objects. 
+		moreFigures.add(rect1);
+		moreFigures.add(rect2);
+		//But this convenience comes at a price. 
+		//Each time the Vector is called upon to grow, it doubles in size, 
+		//so make sure to set the initial capacity of your vector a bit higher 
+		//than the maximum number of objects you anticipate it will hold.
+		moreFigures.add(rect3);
+		moreFigures.add(oval1);
+		moreFigures.add(oval2);
+		moreFigures.add(oval3);
+		moreFigures.add(myPoint);		
+		//When we loop through the moreFigures Vector, we are no longer dealing with an array, 
+		//so we can't use the length constant from an array. We have to use the Vector's size() method, 
+		//which gives us the number of objects in the Vector.
+		for (int i=0; i<moreFigures.size(); i++){
+			System.out.println("Element " + i + " is " + moreFigures.elementAt(i));
+		}
+		
+	}
+	
+}
