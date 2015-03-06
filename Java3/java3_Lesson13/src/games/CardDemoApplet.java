4fbd93552d4257790bf4275a26f475ead99e1b94
bbachmey <bbachmey@gmail.com>
2015-03-06 16:39:37 -0500

    fix the flicker

--- Java3/java3_Lesson13/src/games/CardDemoApplet.java
+++ Java3/java3_Lesson13/src/games/CardDemoApplet.java
@@ -3,18 +3,19 @@ package games;
 import java.util.List;
 import javax.swing.*;
 import java.applet.Applet;
-import java.awt.Graphics;                                    
+import java.awt.*; 
 
-public class CardDemoApplet extends Applet {
- 
+public class CardDemoApplet extends Applet{
+    Graphics bufferGraphics;
+    Image doubleBuffer;
     private static RealCards[] _deck = new RealCards[52];
     CardTable table;
 
     public void init() {
-        resize(400,400);
+        resize(400,400);		 
         makeCards();
         table = new CardTable(_deck);  
-        add(table);
+        add(table);	     
     }
 	 
     public void makeCards(){
@@ -29,7 +30,7 @@ public class CardDemoApplet extends Applet {
             RealCards card = new RealCards(each);
             card.moveTo(xPos, yPos);
             _deck[n] = card;
-
+ 
             //... Update local vars for next card.
             xPos += 5;
             yPos += 4;
@@ -37,7 +38,23 @@ public class CardDemoApplet extends Applet {
         }
     }
 
+    public void update (Graphics g){
+        if (doubleBuffer == null)
+        {
+            doubleBuffer = createImage(this.getSize().width, this.getSize().height);
+            bufferGraphics = doubleBuffer.getGraphics();
+        }
+
+        bufferGraphics.setColor(getBackground());
+        bufferGraphics.fillRect(0,0,this.getSize().width, this.getSize().height);
+
+        bufferGraphics.setColor(getForeground());
+
+        table.paintComponent(bufferGraphics);
+        g.drawImage(doubleBuffer, 0,0,this);
+    }
+	 
     public void paint(Graphics g) {
-        table.paintComponent(g);
+        update(g); 
     }
 }
