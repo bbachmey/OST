5530078106f770c16106bf328ad175c6c7eef9c9
bbachmey <bbachmey@gmail.com>
2015-06-11 13:06:32 -0400

    add java 4 lesson 7

---
+++ Java4/java4_Lesson7/src/exceptionTesting/FileFetcher.java
@@ -0,0 +1,40 @@
+package exceptionTesting;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class FileFetcher {
+    FileReader myFile;
+    BufferedReader in;
+    String aLine =""; 
+
+    public void getHomework() {
+        try 
+        {
+            myFile = new FileReader("homework.txt");  // create a Reader for a file
+            System.out.println("I did get here");
+            in = new BufferedReader(myFile);        // wrap the FileReader in a class that lets us manipulate it
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Can't find the file, but keep going anyway--allows for future problems!");
+        }
+        
+        while (aLine != null){
+            try {
+                aLine = in.readLine();
+            } 
+            catch(IOException e){
+                System.out.println("Now we have some other I/O problem");
+            }
+            if (aLine !=null) System.out.println(aLine);  // we had another readLine after the check for null
+        }  
+        // later, we will do something more here
+    }
+
+    public static void main(String [] args){
+        FileFetcher testMe = new FileFetcher();
+        testMe.getHomework();
+    }
+}
