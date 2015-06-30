a4c575e690ff89d2d4ef1c5a3d94258030af08a2
bbachmey <bbachmey@gmail.com>
2015-06-29 19:16:50 -0500

    finish java 4 lesson 12a

--- Java4/java4_Lesson12a/src/db/PhoneBook.java
+++ Java4/java4_Lesson12a/src/db/PhoneBook.java
@@ -2,11 +2,15 @@ package db;
 
 public class PhoneBook {
 
-    public static void main ( String[] args ) {                                    // args[0] must be the username and 
-                                                                                   // args[1] must be the password to connect to the mysql database
-        DatabaseManager databaseManager = new DatabaseManager(args[0], args[1] );  // Create the database manager.
-    
-        UserInterface userInterface = new UserInterface(databaseManager );         // Create access for user input.
+    public PhoneBook(){
+        String [] info = PasswordDialog.login(this);                              // static login so can call from class
+        DatabaseManager databaseManager = new DatabaseManager(info[0], info[1]);  // Create the database manager and pass login info.
+        UserInterface userInterface = new UserInterface(databaseManager );        // Create access for user input
         userInterface.start();
     }
+   
+    public static void main ( String[] args ) {                                         // instantiate to start                                                                                       // args[1] must be the password to connect to the mysql database
+        PhoneBook myApp = new PhoneBook();     
+    }
 }
+
